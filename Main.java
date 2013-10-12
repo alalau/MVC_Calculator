88b263b09e433c77b0ea9811f832a9850b018fa1
Aaron La Lau <aaron.lalau@gmail.com>
2013-10-12 12:16:36 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package calculator_mvc;
+
+public class Main {
+	public static void main(String [] args) {
+		Model model = new Model();
+        View view = new View(); 
+        new Controller(model,view);
+	}
+}
